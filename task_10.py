81c773c64e40cb902aa0488280064ba42ac1bc7f
Denis <makrotheeast@gmail.com>
2024-01-09 17:54:14 +0300

    Solved task 10

--- task_10.py
+++ task_10.py
@@ -4,10 +4,10 @@ from typing import final
 class Parent():
     @final # декоратор даст понять type cheker'ам о том, что метод нельзя переопределять
     # Но переопределение метода в наследнике не приведет ни к какой ошибке
-    def method1(self):
+    def method(self):
         print('Parent method')
 
 
 class Child(Parent):
-    def method1(self):
+    def method(self):
         print('Child method')
